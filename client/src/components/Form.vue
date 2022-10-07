aa97041ca50f31546c362ed4e9f89beccaf7af9b
Vika Marina <viktoriamv.0204@gmail.com>
2022-10-07 23:19:33 +0300

    fix(README)move read me to main folder

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -1,10 +1,34 @@
 <template>
-  <form>
-    <VInput />
+  <form
+      :class="$style.form"
+      @submit.prevent="handleSubmit"
+      @keydown.enter.prevent="handleSubmit"
+  >
+    <h1>Add new alias</h1>
 
-    <VInput />
+    <VInput
+        v-model="values.alias"
+        :error="errors.alias"
+        label="Alias"
+        @input="errors.alias = ''"
+    />
 
-    <button type="submit">Submit</button>
+    <VInput
+        v-model="values.full_link"
+        :error="errors.full_link"
+        label="Full link"
+        @input="errors.full_link = ''"
+    />
+
+    <button type="submit" :class="$style.button">Submit</button>
+
+    <transition name="fade">
+      <div v-if="resultMessage"
+           :class="$style.result"
+      >
+        {{ resultMessage }}
+      </div>
+    </transition>
   </form>
 </template>
 
@@ -37,12 +61,85 @@ export default {
 
       resultMessage: '',
     };
+  },
+
+  methods: {
+    async handleSubmit() {
+      const validate = () => {
+        let isValidated = true;
+
+        if (!this.values.alias) {
+          this.errors.alias = 'Required field';
+
+          isValidated = false;
+        }
+
+        if (!this.values.full_link) {
+          this.errors.full_link = 'Required field';
+
+          isValidated = false;
+        }
+
+        return isValidated;
+      };
+
+      if (!validate()) {
+        return;
+      }
+
+      try {
+        const response = await fetch('http://localhost:3000/alias', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({alias: this.values.alias, link: this.values.full_link})
+        })
+
+        if(response.ok) {
+          this.resultMessage = 'Successfully send';
+        } else {
+          this.resultMessage = 'An error has occurred';
+        }
+
+        this.afterSubmit();
+      } catch(e) {
+        console.warn('Form/handleSubmit:', e);
+      }
+    },
+
+    afterSubmit() {
+      this.values.alias = '';
+      this.values.full_link = '';
+
+      setTimeout(() => {
+        this.resultMessage = '';
+      }, 3000)
+    },
   }
 };
 </script>
 
 <style lang="scss" module>
-  .read-the-docs {
-    color: #888;
+  .form{
+    position: relative;
+  }
+
+  .button {
+    margin-top: 32px;
+  }
+
+  .result {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: #e9ccfc;
+    font-size: 26px;
   }
 </style>
--- client/src/components/VInput.vue
+++ client/src/components/VInput.vue
@@ -1,15 +1,108 @@
 <template>
-  <input :class="$style.input" />
+  <div :class="$style.input">
+    <label :class="$style.label" v-html="label"></label>
+
+    <input
+        v-model="lazyValue"
+        v-bind="$attrs"
+        type="text"
+        :name="name"
+        :class="$style.inner"
+        @input="onInput"
+    />
+
+    <div :class="$style.error" v-html="error"></div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'VInput',
+
+    props: {
+      name: {
+        type: String,
+        default: '',
+      },
+
+      value: {
+        type: [String, Number],
+        default: '',
+      },
+
+      label: {
+        type: String,
+        default: '',
+      },
+
+      error: {
+        type: [String, Boolean],
+        default: '',
+      },
+    },
+
+    watch: {
+      value(val) {
+        if (val !== this.lazyValue) {
+          let newValue = val;
+
+          this.lazyValue = newValue;
+          this.$emit('input', newValue);
+        }
+      },
+    },
+
+    data() {
+      return {
+        lazyValue: '',
+      };
+    },
+
+    created() {
+      this.lazyValue = this.value;
+    },
+
+    methods: {
+      onInput() {
+        this.$emit('input', this.lazyValue);
+      },
+    },
   };
 </script>
 
 <style lang="scss" module>
   .input {
+    position: relative;
+    width: 400px;
+    font-size: 20px;
+
+    &:not(:first-child) {
+      margin-top: 32px;
+    }
+  }
+
+  .label {
+    display: block;
+    margin-bottom: 10px;
+    font-size: 20px;
+    font-weight: 500;
+    text-align: start;
+  }
+
+  .inner {
     width: 100%;
+    padding: 20px;
+    border: 1px solid grey;
+    font-size: inherit;
+    transition: all .3s ease;
+    outline: none;
+  }
+
+  .error {
+    position: absolute;
+    bottom: -24px;
+    left: 0;
+    font-size: 14px;
+    color: red;
   }
 </style>
